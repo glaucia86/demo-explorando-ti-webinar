b7fd969a7ed3c7bbb55e42395b9d9c5050d70d5f
glaucia86 <glaucia_lemos86@hotmail.com>
2019-01-22 23:04:16 -0200

    Importando o modulo 'slim-loading-bar' no arquivo 'app.module.ts'

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { EmpEditComponent } from './emp-edit/emp-edit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
