116f6f6ecf67effb28ecce8517f60ff792bedd86
glaucia86 <glaucia_lemos86@hotmail.com>
2019-01-24 00:25:31 -0200

    Desenvolvimento do modelo da classe 'Todo' no arquivo 'todo.js'

--- api/models/todo.js
+++ api/models/todo.js
@@ -4,3 +4,9 @@
  * Data: 24/01/2019
  * Author: Glaucia Lemos
  */
+
+const mongoose = require('mongoose');
+
+module.exports = mongoose.model('Todo', {
+  texto: { type: String, default: '' },
+});
