1baabc99ebd9d91ce60deff20d1737c097c68030
glaucia86 <glaucia_lemos86@hotmail.com>
2019-01-24 01:15:42 -0200

    Desenvolvimento da integração da api back com o front no arquivo 'main.js'

---
+++ front-end/js/controllers/main.js
@@ -0,0 +1,45 @@
+/**
+ * Arquivo: js/main.js
+ * Descrição: Arquivo responsável pela integração da api back com o Front
+ * Data: 24/01/2019
+ * Author: Glaucia Lemos
+ */
+
+angular.module('todoController', [])
+
+  .controller('mainController', ['$scope', '$http', 'Todos', function ($scope, $http, Todos) {
+    $scope.formData = {};
+    $scope.loading = true;
+
+    // Retornar os 'Todos':
+    Todos.get()
+      .success((data) => {
+        $scope.todos = data;
+        $scope.loading = false;
+      });
+
+    // Adicionar 'Todo':
+    $scope.createTodo = function () {
+      if ($scope.formData.texto !== undefined) {
+        $scope.loading = true;
+
+        Todos.create($scope.formData)
+          .success((data) => {
+            $scope.loading = false;
+            $scope.formData = {};
+            $scope.todos = data;
+          });
+      }
+    };
+
+    // Excluir 'Todo':
+    $scope.deleteTodo = function (id) {
+      $scope.loading = true;
+
+      Todos.delete(id)
+        .success((data) => {
+          $scope.loading = false;
+          $scope.todos = data;
+        });
+    };
+  }]);
---
+++ front-end/js/core.js
@@ -0,0 +1,8 @@
+/**
+ * Arquivo: js/core.js
+ * Descrição: Arquivo responsável pela execução principal do Angular
+ * Data: 23/01/2019
+ * Author: Glaucia Lemos
+ */
+
+angular.module('todoList', ['todoController', 'todoService']);
