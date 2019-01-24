226b8f064e81b5539fcecc6c9c75f120e614de7b
glaucia86 <glaucia_lemos86@hotmail.com>
2019-01-23 23:13:47 -0200

    Definindo a estrutura inicial da aplicação

---
+++ front-end/core.js
@@ -0,0 +1,6 @@
+/**
+ * Arquivo: core.js
+ * Descrição: Arquivo responsável por toda a lógica do AngularJs
+ * Data: 23/01/2019
+ * Author: Glaucia Lemos
+ */
---
+++ server.js
@@ -0,0 +1,6 @@
+/**
+ * Arquivo: server.js
+ * Descrição: Arquivo responsável por toda a configuração do node.js da aplicação.
+ * Data: 23/01/2019
+ * Author: Glaucia Lemos
+ */
